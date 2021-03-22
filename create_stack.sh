9af69ed48c58728c8c024176b6f3c18e101d5951
Andras Garzo <garzoand@gmail.com>
2021-03-22 16:12:44 -0700

    cm

--- create_stack.sh
+++ create_stack.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-aws --region us-east-1 cloudformation update-stack \
-    --stack-name studio \
-    --template-body file://studio.yaml \
-    --parameters 'ParameterKey=DomainName,ParameterValue=my-sm-domain2' 'ParameterKey=VpcId,ParameterValue=vpc-493a0433' 'ParameterKey=Subnets,ParameterValue="subnet-14f21072"' \
-    --capabilities CAPABILITY_NAMED_IAM
-
-
+#aws --region us-east-1 cloudformation update-stack \
+#    --stack-name studio \
+#    --template-body file://studio.yaml \
+#    --parameters 'ParameterKey=DomainName,ParameterValue=my-sm-domain2' 'ParameterKey=VpcId,ParameterValue=vpc-493a0433' 'ParameterKey=Subnets,ParameterValue="subnet-14f21072"' \
+#    --capabilities CAPABILITY_NAMED_IAM
 #     --parameters 'ParameterKey=DomainName,ParameterValue=my-sm-domain2' 'ParameterKey=VpcId,ParameterValue=vpc-493a0433' 'ParameterKey=Subnets,ParameterValue="subnet-14f21072,subnet-e06f80bf,subnet-863c8ecb"' \
+
+aws --region us-east-1 cloudformation update-stack \
+    --stack-name studio-provision \
+    --template-body file://studio_product.yaml \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --parameters 'ParameterKey=LaunchPrincipal,ParameterValue="arn:aws:iam::163619147629:user/admin"'
