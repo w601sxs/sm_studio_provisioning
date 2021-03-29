b88e92711a47a73256d89c676a3b64eacec093eb
Andras Garzo <garzoand@gmail.com>
2021-03-29 09:26:01 -0700

    cm

--- stack_profile.sh
+++ stack_profile.sh
@@ -7,7 +7,6 @@
 #     --parameters 'ParameterKey=DomainName,ParameterValue=my-sm-domain2' 'ParameterKey=VpcId,ParameterValue=vpc-493a0433' 'ParameterKey=Subnets,ParameterValue="subnet-14f21072,subnet-e06f80bf,subnet-863c8ecb"' \
 
 aws --region us-east-1 cloudformation create-stack \
-    --stack-name studio-provision \
-    --template-body file://studio_product.yaml \
-    --capabilities CAPABILITY_NAMED_IAM \
-    --parameters 'ParameterKey=LaunchPrincipal,ParameterValue="arn:aws:iam::163619147629:user/admin"'
+    --stack-name studio-profile \
+    --template-body file://user_profile.yaml \
+    --parameters 'ParameterKey=DomainId,ParameterValue=d-qmxahw4hwhbn' 'ParameterKey=UserId,ParameterValue=sagemaker-user2' 'ParameterKey=ExecutionRole,ParameterValue=SageMakerTeamExecutionRole'
