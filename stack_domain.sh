04ff780cc7c522fa5b0dcef1b98993652380b7ab
Andras Garzo <garzoand@gmail.com>
2021-03-29 15:17:24 -0700

    test scripts added

--- stack_domain.sh
+++ stack_domain.sh
@@ -6,8 +6,8 @@
 #    --capabilities CAPABILITY_NAMED_IAM
 #     --parameters 'ParameterKey=DomainName,ParameterValue=my-sm-domain2' 'ParameterKey=VpcId,ParameterValue=vpc-493a0433' 'ParameterKey=Subnets,ParameterValue="subnet-14f21072,subnet-e06f80bf,subnet-863c8ecb"' \
 
-aws --region us-east-2 cloudformation update-stack \
-    --stack-name sagemaker-studio \
-    --template-body file://launch_template.yaml \
+aws --region us-east-2 cloudformation create-stack \
+    --stack-name studio-domain \
+    --template-body file://product_studio_domain.yaml \
     --capabilities CAPABILITY_NAMED_IAM \
-    --parameters 'ParameterKey=LaunchPrincipal,ParameterValue="arn:aws:iam::163619147629:user/admin"'
+    --parameters 'ParameterKey=VpcId,ParameterValue=vpc-0cc239f07f70ac641' 'ParameterKey=Subnets,ParameterValue=subnet-0bf9ad3bb4335e57f' 
--- stack_user_profile.sh
+++ stack_user_profile.sh
@@ -6,7 +6,8 @@
 #    --capabilities CAPABILITY_NAMED_IAM
 #     --parameters 'ParameterKey=DomainName,ParameterValue=my-sm-domain2' 'ParameterKey=VpcId,ParameterValue=vpc-493a0433' 'ParameterKey=Subnets,ParameterValue="subnet-14f21072,subnet-e06f80bf,subnet-863c8ecb"' \
 
-aws --region us-east-1 cloudformation create-stack \
+aws --region us-east-2 cloudformation create-stack \
     --stack-name studio-profile \
-    --template-body file://user_profile.yaml \
-    --parameters 'ParameterKey=DomainId,ParameterValue=d-qmxahw4hwhbn' 'ParameterKey=UserId,ParameterValue=sagemaker-user2' 'ParameterKey=ExecutionRole,ParameterValue=SageMakerTeamExecutionRole'
+    --template-body file://product_user_profile.yaml \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --parameters 'ParameterKey=DomainId,ParameterValue=' 'ParameterKey=UserProfileName,ParameterValue=user1' 
