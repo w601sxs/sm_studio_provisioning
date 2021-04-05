4693cd0f4906efa23d7352480fb0bb765d1d0512
Andras Garzo <garzoand@gmail.com>
2021-04-05 12:11:50 -0700

    cm

--- stack_domain.sh
+++ stack_domain.sh
@@ -6,8 +6,8 @@
 #    --capabilities CAPABILITY_NAMED_IAM
 #     --parameters 'ParameterKey=DomainName,ParameterValue=my-sm-domain2' 'ParameterKey=VpcId,ParameterValue=vpc-493a0433' 'ParameterKey=Subnets,ParameterValue="subnet-14f21072,subnet-e06f80bf,subnet-863c8ecb"' \
 
-aws --region us-east-2 cloudformation update-stack \
-    --stack-name studio-domain \
+aws --region eu-west-2 cloudformation create-stack \
+    --stack-name SC-683059238010-pp-6au57bviwqfxm \
     --template-body file://product_studio_domain.yaml \
     --capabilities CAPABILITY_NAMED_IAM \
-    --parameters 'ParameterKey=VpcId,ParameterValue=vpc-0cc239f07f70ac641' 'ParameterKey=Subnets,ParameterValue=subnet-0bf9ad3bb4335e57f' 
+    --parameters 'ParameterKey=VpcId,ParameterValue=vpc-de5475b6' 'ParameterKey=Subnets,ParameterValue=subnet-61381308' 
