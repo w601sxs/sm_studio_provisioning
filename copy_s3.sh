e5eb665b3ab6f5e6f8100f2e04ca7c137c8b5d41
Andras Garzo <garzoand@gmail.com>
2021-04-19 13:10:35 -0700

    PassRole fix

--- copy_s3.sh
+++ copy_s3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-BUCKET_NAME=studio-provisioning-aggarzo
+BUCKET_NAME=studio-provisioning-templates-aggarzo
 aws s3 cp launch_template.yaml s3://$BUCKET_NAME/launch.template
 aws s3 cp product_studio_domain.yaml s3://$BUCKET_NAME/product_studio_domain.template
 aws s3 cp product_user_profile.yaml s3://$BUCKET_NAME/product_user_profile.template
